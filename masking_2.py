31b2f7c8bd4b3ec86343f9530d0e6a656e903667
Okenla Qahhar <80525719+kenzyxriah@users.noreply.github.com>
2025-05-12 03:36:05 +0100

    Add files via upload

--- masking_2.py
+++ masking_2.py
@@ -3,9 +3,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 
-def extract_signature(image_path: str, output_path: str=None, blur_kernel=(5,5)):
+def extract_signature(image_path, output_path=None, blur_kernel=(5,5)):
 
-    # Read the image as an np.ndarray
+    # Read the image as 
     img = cv2.imread(image_path)
     if img is None:
         print(f"Error: Could not read image at {image_path}")
@@ -13,7 +13,7 @@ def extract_signature(image_path: str, output_path: str=None, blur_kernel=(5,5))
     
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
-    # Convert BGR Image to GrayScale
+    # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     # Apply Gaussian blur to reduce noise
@@ -29,27 +29,35 @@ def extract_signature(image_path: str, output_path: str=None, blur_kernel=(5,5))
         2
     )
     
+    # # thresholds for RBG values (when masking our actual image)
+    # lower, higher = (np.array([50,50,50]), 
+    #                  np.array([250,250,250]))
     
-    # Find contours
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # thresholds for RBG values
+    lower, higher = 50,255
     
     # Create a mask for the image
-    mask = np.zeros_like(thresh)
+    mask = cv2.inRange(thresh, lower, higher)
     
-    # Filter out and draw only good enough contours by area 
-    #normally we'd just do, 
-    # cv2.drawContours(mask, contours, -1, 255, -1)
+    # Find contours
+    contours, _ = cv2.findContours(mask, 
+                        cv2.RETR_EXTERNAL, 
+                        cv2.CHAIN_APPROX_NONE) # could be approx_simple too
+    
+
+    
+    # Filter out and draw only good enough contours 
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > 50:  # Adjust this threshold as needed
             cv2.drawContours(
-                    image=mask,          # Where to draw
-                    contours=[contour],  # The contour(s) to draw
-                    contourIdx=-1,       # -1 means draw **all** contours in the list
-                    color=255,           # Draw color: 255 is white (in grayscale)
-                    thickness=-1         # -1 means **fill** the contour
-                )
-    
+                        image=mask,          
+                        contours=[contour],  
+                        contourIdx=-1,       
+                        color=255,           # Draw color: 255 is white (in grayscale)
+                        thickness=-1         
+                    )
+                        
     
     if output_path:
         # Save the result
